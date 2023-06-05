b46ea33e93d03c36bb37eee936d468ce28a614c3
Chris Crane <chrisjcrane@gmail.com>
2023-06-05 13:02:14 -0600

    create user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,11 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    phone_number { "MyString" }
+    name { Faker::TvShows::VentureBros.character }
+    phone_number { Faker::PhoneNumber.cell_phone }
+    # will return in any of the following formats
+    # 333-333-3333
+    # (333) 333-3333
+    # 1-333-333-3333
+    # 333.333.3333
   end
 end
