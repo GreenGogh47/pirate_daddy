9798ed9b9cfe78db430c22331fe71cb4ed087855
Brad Dunlap <braddunlapdpt@gmail.com>
2023-06-05 15:21:38 -0400

    edits

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -2,7 +2,7 @@
 require 'twilio-ruby'
 
 Twilio.configure do |config|
-  config.account_sid = AC99942bb9597b421571e7090d26c5512c
-  config.auth_token = fa823fa0b3400cc33ebf222b4c41c166
-  config.phone_number = +18885063054
+  config.account_sid = 'AC99942bb9597b421571e7090d26c5512c'
+  config.auth_token = 'fa823fa0b3400cc33ebf222b4c41c166'
+  config.phone_number = '+18885063054'
 end
